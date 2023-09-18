0369d2ee997d3e64f23458f9d5586d995d5535dd
PauloMiguel13000git config --global user.name PauloMiguel13000 <guell.28@hotmail.com>
2023-09-18 08:06:51 -0300

    Iniciando components

--- apresentar/src/App.js
+++ apresentar/src/App.js
@@ -1,35 +1,15 @@
 import React, {Component} from 'react';
 
-
+import Membro from './components/Membro'
 class App extends Component {
-    constructor (props){
-super(props);
-this.state = {
-    hora: '00:00:00'
-};
-}
+    render(){
+        return(
+            <div>
+          <Membro nome="visit"></Membro>
+        
 
-componentDidMount(){
-    setInterval(() => {
-this.setState
-({hora: new Date().toLocaleTimeString()})
-    }, 1000);
-}
-
-componentDidUpdate(){
-    console.log('Atualizou')
-}
-
-
-   render(){
-    return(
-<div>
-
-<h1>Meu projeto {this.state.hora}</h1>
-
-
-</div>
-    );
-   }
+            </div>
+        );
+    }
 }
 export default App;
