43ab42b1e44a4f5d0ebe5529e3fbb161506138e0
PauloMiguel13000git config --global user.name PauloMiguel13000 <guell.28@hotmail.com>
2023-09-22 08:25:44 -0300

    praticando formuarios

--- apresentar/src/App.js
+++ apresentar/src/App.js
@@ -1,15 +1,54 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
-import Membro from './components/Membro'
-class App extends Component {
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            senha:'',
+            sexo:'Masculino'
+        }
+        this.trocaEmail = this.trocaEmail.bind(this);
+        this.trocaSexo = this.trocaSexo.bind(this);
+    }
+    trocaSexo(e){
+        let valorDigitado = e.target.value;
+        this.setState({sexo: valorDigitado})
+    }
+    trocaEmail(e){
+        let valorDigitado = e.target.value;
+        this.setState({email: valorDigitado});
+    }
     render(){
         return(
-            <div>
-          <Membro nome="visit"></Membro>
-        
 
-            </div>
+            <div>  
+                <h2>Login</h2>
+            Email:
+            <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail}></input>    
+             <br></br>
+                 Senha:
+                 <input type="password" name="senha"
+                  value={this.state.senha}
+                  onChange={(e) => this.setState({senha: e.target.value})}></input>
+<br></br>
+                Sexo:
+                <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Feminino">Feminino</option>
+                </select>
+                 <div>
+                    <h3>{this.state.email}</h3>
+                    <h3>{this.state.senha}</h3>
+                    <h3>{this.state.sexo}</h3>
+                 </div>
+                 </div>
         );
     }
+
+
 }
+
+
+
 export default App;
